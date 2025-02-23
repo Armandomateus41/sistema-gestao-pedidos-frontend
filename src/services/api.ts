dca466c8f4cabec87ca881087473fefd5b55d70f
Armandomateus41 <armandomateus41@gmail.com>
2025-02-23 14:00:55 -0300

    atualizacao final

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,21 @@
-// src/services/api.ts
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api', // Certifique-se que o backend usa essa URL
-  timeout: 5000, // Define um timeout para as requisições
+  baseURL: 'http://127.0.0.1:8000/api',
+  timeout: 5000,
+});
+
+// Interceptor para adicionar o token ao header
+api.interceptors.request.use((config) => {
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
